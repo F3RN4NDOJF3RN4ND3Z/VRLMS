3735f7d00abd02925d3388c21050051b8992bcf8
Fernando Javier Fernandez <yofercho@gmail.com>
2019-08-20 07:19:36 -0500

    Se añaden controles al video

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import {ReactInstance,Surface} from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
     // Add custom options here
     fullScreen: true,
+
     ...options,
   });
 
@@ -15,7 +16,10 @@ function init(bundle, parent, options = {}) {
     r360.createRoot('VRLMS', { /* initial props */ }),
     r360.getDefaultSurface()
   );
-
+  const s = r360.getDefaultSurface();
+  s.setShape(Surface.SurfaceShape.Cylinder );
+  s.resize(1000, 900);
+  r360.compositor.setCursorVisibility('visible');
   // Load the initial environment
   r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
 }
